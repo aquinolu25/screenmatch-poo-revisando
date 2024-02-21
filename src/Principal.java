0c98b938f514014c8b591b58dbb435e02ca94d0e
Luiz Aquino <quinnpw@live.com>
2024-02-20 21:14:58 -0300

    att encapsulamento

--- src/Principal.java
+++ src/Principal.java
@@ -1,18 +1,19 @@
+import br.com.java.screenmatch.modelos.Filme;
+
 public class Principal {
     public static void main(String[] args) {
 
         Filme meuFilme = new Filme();
         
-        meuFilme.nome = "Saltburn";
-        meuFilme.anoDeLancamento = 2022;
-        meuFilme.duracaoEmMinutos = 180;
+        meuFilme.setNome("Saltburn");
+        meuFilme.setAnoDeLancamento(2022);
+        meuFilme.setDuracaoEmMinutos(180);
 
         meuFilme.exibeFichaTecnica();
         meuFilme.avalia(10);
         meuFilme.avalia(8);
         meuFilme.avalia(7);
-        System.out.println(meuFilme.somaAvaliacao);
-        System.out.println(meuFilme.totalDeAvalicoes);
+        System.out.println("Total de avaliações:" + meuFilme.getTotalDeAvalicoes());
         System.out.println(meuFilme.pegaMedia());
     }
 }

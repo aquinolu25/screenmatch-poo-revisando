54970b8c45b1f0645e0ce1ef473185985b5a7a6f
Luiz Aquino <quinnpw@live.com>
2024-02-21 11:07:32 -0300

    att herança between film and serie

--- src/Principal.java
+++ src/Principal.java
@@ -1,9 +1,11 @@
 import br.com.java.screenmatch.modelos.Filme;
+import br.com.java.screenmatch.modelos.Serie;
+import br.com.java.screenmatch.modelos.Titulo;
 
 public class Principal {
     public static void main(String[] args) {
 
-        Filme meuFilme = new Filme();
+        Titulo meuFilme = new Titulo();
         
         meuFilme.setNome("Saltburn");
         meuFilme.setAnoDeLancamento(2022);
@@ -15,5 +17,12 @@ public class Principal {
         meuFilme.avalia(7);
         System.out.println("Total de avaliações:" + meuFilme.getTotalDeAvalicoes());
         System.out.println(meuFilme.pegaMedia());
+
+        Serie minhaSerie = new Serie();
+        minhaSerie.setNome("The White lotus");
+        minhaSerie.setAnoDeLancamento(2021);
+        minhaSerie.exibeFichaTecnica();
+        minhaSerie.setTemporadas(2);
+        minhaSerie.setEpisodiosPorTemporada(8);
     }
 }
---
+++ src/br/com/java/screenmatch/modelos/Titulo.java
@@ -0,0 +1,60 @@
+package br.com.java.screenmatch.modelos;
+
+public class Titulo {
+    private String nome;
+    private int anoDeLancamento;
+    private boolean incluidoNoPlano;
+    private double somaAvaliacao;
+    private int totalDeAvalicoes;
+    private int duracaoEmMinutos;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public int getAnoDeLancamento() {
+        return anoDeLancamento;
+    }
+
+    public boolean isIncluidoNoPlano() {
+        return incluidoNoPlano;
+    }
+
+    public int getDuracaoEmMinutos() {
+        return duracaoEmMinutos;
+    }
+
+    public int getTotalDeAvalicoes() {
+        return totalDeAvalicoes;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public void setAnoDeLancamento(int anoDeLancamento) {
+        this.anoDeLancamento = anoDeLancamento;
+    }
+
+    public void setIncluidoNoPlano(boolean incluidoNoPlano) {
+        this.incluidoNoPlano = incluidoNoPlano;
+    }
+
+    public void setDuracaoEmMinutos(int duracaoEmMinutos) {
+        this.duracaoEmMinutos = duracaoEmMinutos;
+    }
+
+    public void exibeFichaTecnica() {
+        System.out.println("Nome do titulo: " + nome);
+        System.out.println("Ano de lançamento: " + anoDeLancamento);
+    }
+
+    public void avalia (double nota) {
+        somaAvaliacao += nota;
+        totalDeAvalicoes++;
+    }
+
+    public double pegaMedia() {
+        return somaAvaliacao / totalDeAvalicoes;
+    }
+}

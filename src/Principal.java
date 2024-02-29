4041a7eeeed3e8cc31eca028c62d2bdd500337f7
Luiz Aquino <quinnpw@live.com>
2024-02-29 12:21:39 -0300

    implements toSting to class film

--- src/Principal.java
+++ src/Principal.java
@@ -66,6 +66,8 @@ public class Principal {
         System.out.println("Tamanho da lista de filmes: " + listaDeFilmes.size());
         System.out.println("Primeiro filme da lista: " + listaDeFilmes.get(0).getNome());
         System.out.println("*************************************************************");
+        System.out.println(listaDeFilmes);
+
 
     }
 }
